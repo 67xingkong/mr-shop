a0f50d81bd3d25ea8503fdf3ed50580fcaa9747e
67xingkong <1819197991@qq.com>
2020-08-27 15:14:29 +0800

    测试提交

--- mingrui-shop-parent/mingrui-shop-basics/mingrui-shop-eureka-server/src/main/java/com/baidu/RunEurekaServerApplication.java
+++ mingrui-shop-parent/mingrui-shop-basics/mingrui-shop-eureka-server/src/main/java/com/baidu/RunEurekaServerApplication.java
@@ -16,7 +16,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class RunEurekaServerApplication {
 
     public static void main(String[] args) {
-        System.out.println("asd");
+
         SpringApplication.run(RunEurekaServerApplication.class);
     }
 }
